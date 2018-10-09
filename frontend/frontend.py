f273c53c88b498758b9cc610ccc11a8ca6cce6b6
wit <poowis2011@hotmail.com>
2018-10-09 22:00:25 +0700

    fixed frontend deployment issue

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -7,48 +7,48 @@ import os
 import requests
 from flask_cors import CORS
 
-# HOST = os.getenv("frontend", "localhost")
+HOST = os.getenv("frontend", "localhost")
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route('/')
 def index():
-	r = requests.get("http://sos:5000/debug?get")
+	r = requests.get("http://localhost:5000/debug?get")
 	objects = (r.json()['buckets'])
 	return render_template('index.html', list_example=objects)
 
 @app.route('/<bucketname>/show_all_videos')
 def show(bucketname):
-	r = requests.get(f"http://sos:5000/{bucketname}?list")
+	r = requests.get(f"http://localhost:5000/{bucketname}?list")
 	objects = (r.json()['objects'])
 	return render_template('show.html',
 		display=bucketname+"/display", 
 		bucketname=bucketname, list_example=[(  obj['name'],
-												f"http://queue-wrapper:5001/{bucketname}/{obj['name']}",
-												f"http://frontend:8080/{bucketname}/{obj['name']}/delete")for obj in objects],
-		make_all=f"http://queue-wrapper:5001/{bucketname}"
+												f"http://localhost:5001/{bucketname}/{obj['name']}",
+												f"http://localhost:8080/{bucketname}/{obj['name']}/delete")for obj in objects],
+		make_all=f"http://localhost:5001/{bucketname}"
 		)
 
 @app.route('/<bucketname>/display')
 def display(bucketname):
-	r = requests.get(f"http://sos:5000/{bucketname}?list")
+	r = requests.get(f"http://localhost:5000/{bucketname}?list")
 	objects = (r.json()['objects'])
 	return render_template('display.html',
-		delete_all=f"http://frontend:8080/{bucketname}/delete_all",
+		delete_all=f"http://localhost:8080/{bucketname}/delete_all",
 		show=bucketname+"/show_all_videos", 
 		bucketname=bucketname, 
-		list_example=[(obj['name'],f"http://sos:5000/gifs/{bucketname}_{obj['name']}.gif",f"http://frontend:8080/{bucketname}/{obj['name']}/delete") for obj in objects])
+		list_example=[(obj['name'],f"http://localhost:5000/gifs/{bucketname}_{obj['name']}.gif",f"http://localhost:8080/{bucketname}/{obj['name']}/delete") for obj in objects])
 
 @app.route('/<bucketname>/<objectname>/delete')
 def delete(bucketname, objectname):
-	r = requests.delete(f"http://sos:5000/gifs/{bucketname}_{objectname}.gif?delete")
+	r = requests.delete(f"http://localhost:5000/gifs/{bucketname}_{objectname}.gif?delete")
 	return jsonify({'status': 'OK'})
 @app.route('/<bucketname>/delete_all')
 def delete_all(bucketname):
-	r = requests.get(f"http://sos:5000/{bucketname}?list")
+	r = requests.get(f"http://localhost:5000/{bucketname}?list")
 	objects = (r.json()['objects'])
 	for obj in objects:
-		requests.delete(f"http://sos:5000/gifs/{bucketname}_{obj['name']}.gif?delete")
+		requests.delete(f"http://localhost:5000/gifs/{bucketname}_{obj['name']}.gif?delete")
 	return jsonify({'status': 'OK'})
 
 @app.errorhandler(404)
